3e6f9faf1688c22f8f60c78a694bb381bb04705a
osm3rr <osm3rr@gmail.com>
2022-08-08 20:28:32 -0400

    first window

--- space1.py
+++ space1.py
@@ -1,20 +1,19 @@
 import pygame
 
-# initializate pygame
 pygame.init()
 
-# windows size
+# size
 screen_width = 800
 screen_height = 600
 
 size = ( screen_width, screen_height )
 
-# Display the windows
 screen = pygame.display.set_mode( size )
 
 # game loop
 running = True
 while running:
     for event in pygame.event.get():
+        #print( event )
         if event.type == pygame.QUIT:
             running = False
