31596866944f1b9e1c9746d41ad6aaae65a923ac
osm3rr <osm3rr@gmail.com>
2022-08-19 13:53:24 -0400

    movement mechanics to the enemy and update guide 7

--- space_v8_i.py
+++ space_v8_i.py
@@ -1,5 +1,7 @@
 # v1_1: pygame library
 import pygame 
+# v7_4/6: random library
+import random
 
 # v1_2: initializate pygame
 pygame.init()
@@ -28,11 +30,22 @@ player_y = 480
 # v5_4: add the rate change in x
 player_x_change = 0
 
-# v7_1/3: enemy definition
+# v7_1/6: enemy definition
 enemy_img = pygame.image.load( "alien.png" )
-enemy_x = 370
-enemy_y = 50
-enemy_x_change = 0
+
+#v7_5/6: randoom movement of the enemy in x
+# enemy_x = 370
+enemy_x = random.randint( 0, 800 )
+
+# v7_6/6: randoom movement of the enemy in y
+# enemy_y = 50
+enemy_y = random.randint( 50, 150 )
+
+# ||||||||| v8_1/5: change the value of rate change in "x"
+enemy_x_change = 0.3
+
+# ||||||||| v8_5/5: change the value of rate change in "y"
+enemy_y_change = 40
 
 # v3_2: player function
 # def player():
@@ -42,10 +55,9 @@ enemy_x_change = 0
 def player(x, y):
     screen.blit( player_img, ( x, y ) )
 
-# v7_2/3: enemy function
+# v7_2/6: enemy function
 def enemy(x, y):
     screen.blit( enemy_img, ( x, y ) )
-
 
 # v1_6: Game loop
 running = True
@@ -92,13 +104,30 @@ while running:
     # player boundaries right
     elif player_x >= 736: 
         player_x = 736
+
+    # |||||||| v8_2/5: logic for movement of the enemy
+    enemy_x += enemy_x_change
+    if enemy_x <= 0:
+        enemy_x_change = 0.5
+        # |||||||| v8_4/5: increment the value of the 
+        # "y" coordinate of the enemy
+        enemy_y += enemy_y_change
+
+    #||||||| v8_3/5: size of the enemy is according 
+    # to size of the enemy in px. in this case the enemy
+    # size is 24 px, this is: 800 - 24: 776
+      
+    elif enemy_x >= 776: 
+        enemy_x_change = -0.5
+        # |||||||| v8_5/5: increment the value of the 
+        # "y" coordinate of the enemy
+        enemy_y += enemy_y_change
     
     # v4_2: add the arguments to function
     player( player_x, player_y )
 
-    # v7_3/3: calling to enemy function
+    # v7_3/6: calling to enemy function
     enemy( enemy_x, enemy_y )
     
     # v2_4: update the window 
     pygame.display.update()
-
